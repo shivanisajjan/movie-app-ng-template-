a2a359e319cac0eff811dca7699e5ad7c0a89622
shivanisajjan <shivanisajjan269@gmail.com>
2019-10-17 22:11:51 +0530

    frontend for micro service final

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MovieService } from '../movie.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  public movies =  {};
+  constructor(private _movieService : MovieService,private router: Router) { }
+
+  ngOnInit() {
+    this._movieService.getMovies().subscribe(data => this.movies = data);
+  }
+  toSearch(movieName){
+    this.router.navigate(['./search',{name:movieName}]);
+  }
+  goToMovie(movieId){
+      console.log("shivani");
+    this.router.navigate(['./movie',{id:movieId}]);
+  }
+
+}
